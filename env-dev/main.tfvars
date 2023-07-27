d4dbf8a39d78d3eedc3beb270eeafce052996841
phaneendra <phani.manthena27@gmail.com>
2023-07-27 12:03:46 +0530

    updating ./roboshop-infra

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -3,5 +3,6 @@ default_vpc_id = "vpc-0136e61a0bfd44629"
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
+    subnets_cidr = ["10.0.0.0/17", "10.0.128.0/17"]
   }
 }
--- main.tf
+++ main.tf
@@ -4,4 +4,5 @@ module "network" {
   default_vpc_id = "var.default_vpc_id"
   for_each = var.vpc
   cidr_block = each.value.cidr_block
+  subnets_cidr = each.value.subnets_cidr
 }
