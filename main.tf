78b671a5909373a328cf3c391a1bdd4500c341f3
phaneendra <phani.manthena27@gmail.com>
2023-07-28 14:21:04 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "vpc" {
-  source               = "github.com/Phaneendra-Manthena/tf-module-vpc"
-  env                  = var.env
-  default_vpc_id       = var.default_vpc_id
-  for_each             = var.vpc
-  cidr_block           = each.value.cidr_block
-  public_subnets = each.value.public_subnets
-  private_subnets = each.value.private_subnets
+  source            = "github.com/Phaneendra-Manthena/tf-module-vpc"
+  env               = var.env
+  default_vpc_id    = var.default_vpc_id
+  for_each          = var.vpc
+  cidr_block        = each.value.cidr_block
+  public_subnets    = each.value.public_subnets
+  private_subnets   = each.value.private_subnets
   availability_zone = each.value.availability_zone
-  name                 = each.key
+  name              = each.key
 }
