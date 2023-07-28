cf31ad83c1f5e717cab1414962a06cc295cab041
phaneendra <phani.manthena27@gmail.com>
2023-07-28 17:11:00 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ module "docdb" {
   for_each   = var.docdb
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), "each.value.subnets_name,null),"subnet_ids",null)
     }
-#output "vpc" {
-#  value = module.vpc
-#}
+output "vpc" {
+  value = module.vpc
+}
