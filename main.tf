f7f64e20f21bfcf8dd6e2ba0e255ddb23ada2afb
phaneendra <phani.manthena27@gmail.com>
2023-07-28 17:07:28 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "docdb" {
   env = var.env
   for_each = var.docdb
 #  subnet_ids = lookup(lookup(lookup(lookup(module.vpc,each.value.vpc_name,null),"private_subnet_ids",null),"each.value.subnets_name,null),"subnet_ids",null)
-    subnet_ids = lookup(lookup(lookup(vpc.main.private_subnet_ids, "db", null), "subnet_ids", null), "*")
+    subnet_ids =  lookup(lookup(lookup(lookup(module.private_subnets,"app",null),"subnet_ids",null),"db",null),"subnet_ids",null)
 }
 
 output "vpc" {
