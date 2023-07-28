b1224db71c6ca79f31e229372ed48ca0ac08a40e
phaneendra <phani.manthena27@gmail.com>
2023-07-28 17:06:29 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "vpc" {
 }
 
 module "docdb" {
-  source = "https://github.com/Phaneendra-Manthena/tf-module-docdb.git"
+  source = "github.com/Phaneendra-Manthena/tf-module-docdb.git"
   env = var.env
   for_each = var.docdb
 #  subnet_ids = lookup(lookup(lookup(lookup(module.vpc,each.value.vpc_name,null),"private_subnet_ids",null),"each.value.subnets_name,null),"subnet_ids",null)
