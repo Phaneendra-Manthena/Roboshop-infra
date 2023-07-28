31bce55df87dea289a467a3b0bad28dd1df699f7
phaneendra <phani.manthena27@gmail.com>
2023-07-28 17:33:13 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -15,7 +15,14 @@ module "docdb" {
   env        = var.env
   for_each   = var.docdb
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
+  vpc_id = lookup(lookup(module.vpc,each.value.vpc_name, null),"vpc_id", null)
+  allow_cidr = lookup(lookup(lookup(lookup(module.vpc,each.value.vpc_name, null ),"private_subnet_ids", null),app null),"cidr_block", null)
 }
-output "vpc" {
-  value = module.vpc
-}
+
+
+
+
+
+#output "vpc" {
+#  value = module.vpc
+#}
